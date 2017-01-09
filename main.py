38fe4f8607c9e610378797763a46307cb78171e5
David Lee <jl4397@columbia.edu>
2017-01-09 08:31:58 -0800

    Adding misc scripts

--- main.py
+++ main.py
@@ -60,6 +60,7 @@ def parse_slack_output(slack_rtm_output):
 
 if __name__ == "__main__":
     READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
+
     if slack_client.rtm_connect():
         print("Reviewbot connected and running!")
         while True:
